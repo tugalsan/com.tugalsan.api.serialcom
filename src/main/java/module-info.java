85017eb7df651bb6fc06191de7c0d956ba4b3d03
tugalsan karabacak <tugalsan@gmail.com>
2023-04-05 08:41:01 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module com.tugalsan.api.serialcom {
     requires com.fazecast.jSerialComm;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.thread;
     exports com.tugalsan.api.serialcom.server;
 }
