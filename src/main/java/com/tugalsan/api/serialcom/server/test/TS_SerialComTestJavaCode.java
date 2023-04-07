6e8ff390c2569094dd6936db0b0411ded554ab98
tugalsan karabacak <tugalsan@gmail.com>
2023-04-07 10:30:25 +0300

    .

--- src/main/java/com/tugalsan/api/serialcom/server/test/TS_SerialComTestJavaCode.java
+++ src/main/java/com/tugalsan/api/serialcom/server/test/TS_SerialComTestJavaCode.java
@@ -1,15 +1,36 @@
 package com.tugalsan.api.serialcom.server.test;
 
 import com.tugalsan.api.log.server.*;
-import com.tugalsan.api.serialcom.server.utils.*;
+import com.tugalsan.api.serialcom.server.*;
+import com.tugalsan.api.serialcom.server.utils.TS_SerialComUtils;
 import com.tugalsan.api.thread.server.TS_ThreadWait;
 import java.util.stream.IntStream;
 
-public class TS_SerialComTestUtils {
+public class TS_SerialComTestJavaCode {
 
-    final private static TS_Log d = TS_Log.of(TS_SerialComTestUtils.class);
+    final private static TS_Log d = TS_Log.of(TS_SerialComTestJavaCode.class);
 
-    public static void test() {
+    public static void testBuilder() {
+        d.cr("testBuilder", "------------------------------------------");
+        TS_SerialComBuilder.portFirst()
+                .baudRate_115200().dataBits_8().oneStopBit().parityNone()
+                .onReceived(command -> d.cr("onReceived", command))
+                .use(con -> {
+                    d.cr("con.name()", con.name());
+                    d.cr("con.success_portConnect()", con.success_portConnect());
+                    d.cr("con.success_portPresent()", con.success_portPresent());
+                    d.cr("con.success_portSetup()", con.success_portSetup());
+                    d.cr("con.isConnected()", con.isConnected());
+                    IntStream.range(0, 5).forEach(i -> {
+                        d.cr("send.successful?", con.send("hello"));
+                        d.cr("send.successful?", con.send("naber?"));
+                        TS_ThreadWait.seconds(null, 1);
+                    });
+                });
+    }
+
+    public static void testUtils() {
+        d.cr("testUtils", "------------------------------------------");
         var serialPortList = TS_SerialComUtils.list();
         if (serialPortList.isEmpty()) {
             d.cr("test", "No serial port detected");
@@ -45,5 +66,4 @@ public class TS_SerialComTestUtils {
                 serialPort
         ));
     }
-
 }
