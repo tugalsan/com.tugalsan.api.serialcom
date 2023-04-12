9d9782d8b38a621a2d0aca4285c05d90472b3066
tugalsan karabacak <tugalsan@gmail.com>
2023-04-12 17:04:01 +0300

    .

--- src/main/java/com/tugalsan/api/serialcom/server/TS_SerialComMessageBroker.java
+++ src/main/java/com/tugalsan/api/serialcom/server/TS_SerialComMessageBroker.java
@@ -9,7 +9,7 @@ import java.util.concurrent.Callable;
 
 public class TS_SerialComMessageBroker {
 
-    final private static TS_Log d = TS_Log.of(TS_SerialComMessageBroker.class);
+    final private static TS_Log d = TS_Log.of(true, TS_SerialComMessageBroker.class);
 
     public TS_SerialComMessageBroker(int maxSize) {
         this.maxSize = maxSize;
@@ -45,11 +45,14 @@ public class TS_SerialComMessageBroker {
             replies.removeAll(reply);
             return reply;
         };
+        d.ci("sendTheCommand_and_fetchMeReplyInMaxSecondsOf", "will use TS_ThreadRunAllUntilFirstFail...");
         var run = TS_ThreadRunAllUntilFirstFail.of(Duration.ofSeconds(2), callableReply);
+        d.ci("sendTheCommand_and_fetchMeReplyInMaxSecondsOf", "usage of TS_ThreadRunAllUntilFirstFail successful");
         if (run.resultsNotNull.isEmpty()) {
             run.exceptions.forEach(e -> d.ct("send->" + command, e));
             return null;
         }
+        d.cr("sendTheCommand_and_fetchMeReplyInMaxSecondsOf", "#2");
         return run.resultsNotNull.get(0);
     }
 
