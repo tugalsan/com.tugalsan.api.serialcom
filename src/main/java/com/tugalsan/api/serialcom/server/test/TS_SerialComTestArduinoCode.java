06d3cbbe0d99e221f1ccef53f04c094257fcb0b6
tugalsan karabacak <tugalsan@gmail.com>
2023-04-12 12:54:12 +0300

    .

--- src/main/java/com/tugalsan/api/serialcom/server/test/TS_SerialComTestArduinoCode.java
+++ src/main/java/com/tugalsan/api/serialcom/server/test/TS_SerialComTestArduinoCode.java
@@ -2,62 +2,51 @@ package com.tugalsan.api.serialcom.server.test;
 
 public class TS_SerialComTestArduinoCode {
     /* 
-    //PIN-INIT
-#include "PCF8574.h"
-TwoWire I2Cone = TwoWire(0);
-TwoWire I2Ctwo = TwoWire(1);
-PCF8574 pcf8574_R1(&I2Ctwo, 0x24, 15, 13);
-PCF8574 pcf8574_R2(&I2Ctwo, 0x25, 15, 13);
-PCF8574 pcf8574_R3(&I2Ctwo, 0x21, 15, 13);
-PCF8574 pcf8574_R4(&I2Ctwo, 0x22, 15, 13);
-PCF8574 pcf8574_I1(&I2Cone, 0x24, 4, 5);
-PCF8574 pcf8574_I2(&I2Cone, 0x25, 4, 5);
-PCF8574 pcf8574_I3(&I2Cone, 0x21, 4, 5);
-PCF8574 pcf8574_I4(&I2Cone, 0x22, 4, 5);
+   //USAGE: stringUtils.isInt("ad") -> true/false
+class TA_StringUtils {
+public:
+  TA_StringUtils();
+  bool isInt(String st);
+private:
+};
+TA_StringUtils::TA_StringUtils() {
+}
+bool TA_StringUtils::isInt(String str) {
+  for (byte i = 0; i < str.length(); i++) {
+    if (isDigit(str.charAt(i))) return true;
+  }
+  return false;
+}
+TA_StringUtils stringUtils;
 
-//TIME-INIT
-unsigned long currentTime = millis();  // Current time
-unsigned long previousTime = 0;        // Previous time
-const long timeoutTime = 2000;         // Define timeout time in milliseconds (example: 2000ms = 2s)
-
-//SERIAL-INIT
-int ms_serialWaitUntillConnection = 3000;  //0 for forever, default 3000
-#define serialbufferSize 50
-char serial_lineFound_buffer[serialbufferSize];
-int serial_lineFound_index = 0;
-
-//FLAGS-INIT
-int flag_read_di = 0;
-
-//STRINGTOKENIZER-INIT
 // USAGE
-// StringTokenizer tokens("aa:bb:cc:dd:ee", ":");
+// TA_StringTokenizer tokens("aa:bb:cc:dd:ee", ":");
 // while (tokens.hasNext()) {
 //   Serial.println(tokens.nextToken());
 // }
-class StringTokenizer {
+class TA_StringTokenizer {
 public:
-  StringTokenizer(String str, String del);
-  boolean hasNext();
+  TA_StringTokenizer(String str, String del);
+  bool hasNext();
   String nextToken();
 private:
   String _str;
   String _del;
   int ptr;
 };
-StringTokenizer::StringTokenizer(String str, String del) {
+TA_StringTokenizer::TA_StringTokenizer(String str, String del) {
   _str = str;
   _del = del;
   ptr = 0;
 }
-boolean StringTokenizer::hasNext() {
+bool TA_StringTokenizer::hasNext() {
   if (ptr < _str.length()) {
     return true;
   } else {
     return false;
   }
 }
-String StringTokenizer::nextToken() {
+String TA_StringTokenizer::nextToken() {
   if (ptr >= _str.length()) {
     ptr = _str.length();
     return "";
@@ -75,89 +64,406 @@ String StringTokenizer::nextToken() {
   }
 }
 
-//MAIN-INIT
-void setup() {
-  //MAIN-INIT-PIN
-  for (int i = 0; i <= 7; i++) {
-    pcf8574_I1.pinMode(i, INPUT);
-    pcf8574_I2.pinMode(i, INPUT);
-    pcf8574_I3.pinMode(i, INPUT);
-    pcf8574_I4.pinMode(i, INPUT);
-    pcf8574_R1.pinMode(i, OUTPUT);
-    pcf8574_R2.pinMode(i, OUTPUT);
-    pcf8574_R3.pinMode(i, OUTPUT);
-    pcf8574_R4.pinMode(i, OUTPUT);
-  }
-  pcf8574_I1.begin();
-  pcf8574_I2.begin();
-  pcf8574_I3.begin();
-  pcf8574_I4.begin();
-  pcf8574_R1.begin();
-  pcf8574_R2.begin();
-  pcf8574_R3.begin();
-  pcf8574_R4.begin();
-  for (int i = 0; i <= 7; i++) {
-    pcf8574_R1.digitalWrite(i, HIGH);
-    pcf8574_R2.digitalWrite(i, HIGH);
-    pcf8574_R3.digitalWrite(i, HIGH);
-    pcf8574_R4.digitalWrite(i, HIGH);
-  }
+//TIME
+//USAGE: void loop() { time.next();
+class TA_Time {
+public:
+  TA_Time();
+  void nextLoop();
+  unsigned long current();
+  unsigned long previous();
+  unsigned long delta();
+private:
+  unsigned long _current;
+  unsigned long _previous;
+  unsigned long _delta;
+};
+TA_Time::TA_Time() {
+  _current = millis();
+  nextLoop();
+}
+unsigned long TA_Time::current() {
+  return _current;
+}
+unsigned long TA_Time::previous() {
+  return _previous;
+}
+unsigned long TA_Time::delta() {
+  return _delta;
+}
+void TA_Time::nextLoop() {
+  _previous = _current;
+  _current = millis();
+  _delta = _current - _previous;
+}
+TA_Time time();
 
-  //MAIN-INIT-SERIAL
-  Serial.begin(115200);
-  if (ms_serialWaitUntillConnection == 0) {
+//SERIAL
+#define TA_Serial_BUFFER_SIZE 30
+#define TA_Serial_WAIT_IN_SECONDS 3
+class TA_Serial {
+public:
+  TA_Serial(bool waitUntilConnection, int _baudRate);
+  bool waitUntilConnection();
+  int bufferSize();
+  void setup();
+  bool hasNext();
+  String next();
+private:
+  int _baudRate;
+  bool _waitUntilConnection;
+  int _bufferSize;
+  char _buffer[TA_Serial_BUFFER_SIZE];
+  int _bufferIdx;
+};
+TA_Serial::TA_Serial(bool waitUntilConnection, int baudrate) {
+  _waitUntilConnection = waitUntilConnection;
+  _bufferSize = TA_Serial_BUFFER_SIZE;
+  int _bufferIdx = 0;
+}
+void TA_Serial::setup() {
+  Serial.begin(_baudRate);
+  if (_waitUntilConnection) {
     while (!Serial)
       ;
   } else {
-    while (!Serial && (millis() < ms_serialWaitUntillConnection))
+    while (!Serial && (millis() < TA_Serial_WAIT_IN_SECONDS * 1000))
       ;
   }
-
-  //MAIN-INIT-CARDINFO
-  Serial.print("Starting WebServer on ");
-  Serial.print(String(ARDUINO_BOARD));
-  Serial.print("\n");
-
-  //MAIN-INIT-PRINT-INTERFACE
-  Serial.println("type hello");
 }
-
-//LOOP
-void loop() {
-  //LOOP-HANDLE SERIAL COMMANDS
-  if (serial_lineFound()) serial_lineProcess(serial_lineFound_buffer);
+String TA_Serial::next() {
+  return String(_buffer);
 }
-
-//SERIAL-LINE FINDER
-boolean serial_lineFound() {
-  boolean lineFound = false;
+bool TA_Serial::hasNext() {
+  bool lineFound = false;
   while (Serial.available() > 0) {
-    char charBuffer = Serial.read();
-    if (charBuffer == '\n') {  //command received fully
-      serial_lineFound_buffer[serial_lineFound_index] = 0;
-      lineFound = (serial_lineFound_index > 0);
-      serial_lineFound_index = 0;
-    } else if (charBuffer == '\r') {                                               //ignore
-    } else if (serial_lineFound_index < serialbufferSize && lineFound == false) {  //buffer up char
-      serial_lineFound_buffer[serial_lineFound_index++] = charBuffer;
+    char chr = Serial.read();
+    if (chr == '\r') {         //ignore
+    } else if (chr == '\n') {  //command received fully
+      _buffer[_bufferIdx] = 0;
+      lineFound = _bufferIdx > 0;
+      _bufferIdx = 0;
+    } else if (_bufferIdx < _bufferSize && lineFound == false) {  //buffer up char
+      _buffer[_bufferIdx++] = chr;
     }
   }
   return lineFound;
 }
+TA_Serial serial(true, 115200);
 
-//SERIAL-LINE PROCESSOR
-void serial_lineProcess(char* commandBuffer) {
-  if (strstr(commandBuffer, "hello")) {
-    Serial.println("hello2u2");
-  } else if (commandBuffer[0] == '!') {
-      StringTokenizer tokens(String(commandBuffer), " ");
-      while (tokens.hasNext()) {
-        Serial.println(tokens.nextToken());
-      }
-  } else {
-    Serial.print("I dont understand you. You said: ");
-    Serial.println(commandBuffer);
+//TA_Chip_KinCony_KC868_A32_R1_2
+#include "PCF8574.h"
+TwoWire _I2C_1 = TwoWire(0);
+TwoWire _I2C_2 = TwoWire(1);
+PCF8574 _pcf8574_R1(&_I2C_2, 0x24, 15, 13);
+PCF8574 _pcf8574_R2(&_I2C_2, 0x25, 15, 13);
+PCF8574 _pcf8574_R3(&_I2C_2, 0x21, 15, 13);
+PCF8574 _pcf8574_R4(&_I2C_2, 0x22, 15, 13);
+PCF8574 _pcf8574_I1(&_I2C_1, 0x24, 4, 5);
+PCF8574 _pcf8574_I2(&_I2C_1, 0x25, 4, 5);
+PCF8574 _pcf8574_I3(&_I2C_1, 0x21, 4, 5);
+PCF8574 _pcf8574_I4(&_I2C_1, 0x22, 4, 5);
+class TA_Chip_KinCony_KC868_A32_R1_2 {
+public:
+  TA_Chip_KinCony_KC868_A32_R1_2();
+  String name();
+  void setup();
+  bool getDI_fr1_to32(int pinIdx);
+  bool getDO_fr1_to32(int pinIdx);
+  void setDO_fr1_to32(int pinIdx, bool value);
+private:
+};
+TA_Chip_KinCony_KC868_A32_R1_2::TA_Chip_KinCony_KC868_A32_R1_2() {
+}
+bool TA_Chip_KinCony_KC868_A32_R1_2::getDI_fr1_to32(int pinIdx) {
+  if (pinIdx <= 0) {
+    return false;
   }
+  if (pinIdx <= 8) {
+    if (pinIdx == 1) return _pcf8574_I1.digitalRead(P0) == HIGH;
+    if (pinIdx == 2) return _pcf8574_I1.digitalRead(P1) == HIGH;
+    if (pinIdx == 3) return _pcf8574_I1.digitalRead(P2) == HIGH;
+    if (pinIdx == 4) return _pcf8574_I1.digitalRead(P3) == HIGH;
+    if (pinIdx == 5) return _pcf8574_I1.digitalRead(P4) == HIGH;
+    if (pinIdx == 6) return _pcf8574_I1.digitalRead(P5) == HIGH;
+    if (pinIdx == 7) return _pcf8574_I1.digitalRead(P6) == HIGH;
+    if (pinIdx == 8) return _pcf8574_I1.digitalRead(P7) == HIGH;
+  }
+  if (pinIdx <= 16) {
+    if (pinIdx == 9) return _pcf8574_I2.digitalRead(P0) == HIGH;
+    if (pinIdx == 10) return _pcf8574_I2.digitalRead(P1) == HIGH;
+    if (pinIdx == 11) return _pcf8574_I2.digitalRead(P2) == HIGH;
+    if (pinIdx == 12) return _pcf8574_I2.digitalRead(P3) == HIGH;
+    if (pinIdx == 13) return _pcf8574_I2.digitalRead(P4) == HIGH;
+    if (pinIdx == 14) return _pcf8574_I2.digitalRead(P5) == HIGH;
+    if (pinIdx == 15) return _pcf8574_I2.digitalRead(P6) == HIGH;
+    if (pinIdx == 16) return _pcf8574_I2.digitalRead(P7) == HIGH;
+  }
+  if (pinIdx <= 24) {
+    if (pinIdx == 17) return _pcf8574_I3.digitalRead(P0) == HIGH;
+    if (pinIdx == 18) return _pcf8574_I3.digitalRead(P1) == HIGH;
+    if (pinIdx == 19) return _pcf8574_I3.digitalRead(P2) == HIGH;
+    if (pinIdx == 20) return _pcf8574_I3.digitalRead(P3) == HIGH;
+    if (pinIdx == 21) return _pcf8574_I3.digitalRead(P4) == HIGH;
+    if (pinIdx == 22) return _pcf8574_I3.digitalRead(P5) == HIGH;
+    if (pinIdx == 23) return _pcf8574_I3.digitalRead(P6) == HIGH;
+    if (pinIdx == 24) return _pcf8574_I3.digitalRead(P7) == HIGH;
+  }
+  if (pinIdx <= 32) {
+    if (pinIdx == 25) return _pcf8574_I4.digitalRead(P0) == HIGH;
+    if (pinIdx == 26) return _pcf8574_I4.digitalRead(P1) == HIGH;
+    if (pinIdx == 27) return _pcf8574_I4.digitalRead(P2) == HIGH;
+    if (pinIdx == 28) return _pcf8574_I4.digitalRead(P3) == HIGH;
+    if (pinIdx == 29) return _pcf8574_I4.digitalRead(P4) == HIGH;
+    if (pinIdx == 30) return _pcf8574_I4.digitalRead(P5) == HIGH;
+    if (pinIdx == 31) return _pcf8574_I4.digitalRead(P6) == HIGH;
+    if (pinIdx == 32) return _pcf8574_I4.digitalRead(P7) == HIGH;
+  }
+  return false;
+}
+bool TA_Chip_KinCony_KC868_A32_R1_2::getDO_fr1_to32(int pinIdx) {
+  if (pinIdx <= 0) {
+    return false;
+  }
+  if (pinIdx <= 8) {
+    if (pinIdx == 1) return _pcf8574_R1.digitalRead(P0) == HIGH;
+    if (pinIdx == 2) return _pcf8574_R1.digitalRead(P1) == HIGH;
+    if (pinIdx == 3) return _pcf8574_R1.digitalRead(P2) == HIGH;
+    if (pinIdx == 4) return _pcf8574_R1.digitalRead(P3) == HIGH;
+    if (pinIdx == 5) return _pcf8574_R1.digitalRead(P4) == HIGH;
+    if (pinIdx == 6) return _pcf8574_R1.digitalRead(P5) == HIGH;
+    if (pinIdx == 7) return _pcf8574_R1.digitalRead(P6) == HIGH;
+    if (pinIdx == 8) return _pcf8574_R1.digitalRead(P7) == HIGH;
+  }
+  if (pinIdx <= 16) {
+    if (pinIdx == 9) return _pcf8574_R2.digitalRead(P0) == HIGH;
+    if (pinIdx == 10) return _pcf8574_R2.digitalRead(P1) == HIGH;
+    if (pinIdx == 11) return _pcf8574_R2.digitalRead(P2) == HIGH;
+    if (pinIdx == 12) return _pcf8574_R2.digitalRead(P3) == HIGH;
+    if (pinIdx == 13) return _pcf8574_R2.digitalRead(P4) == HIGH;
+    if (pinIdx == 14) return _pcf8574_R2.digitalRead(P5) == HIGH;
+    if (pinIdx == 15) return _pcf8574_R2.digitalRead(P6) == HIGH;
+    if (pinIdx == 16) return _pcf8574_R2.digitalRead(P7) == HIGH;
+  }
+  if (pinIdx <= 24) {
+    if (pinIdx == 17) return _pcf8574_R3.digitalRead(P0) == HIGH;
+    if (pinIdx == 18) return _pcf8574_R3.digitalRead(P1) == HIGH;
+    if (pinIdx == 19) return _pcf8574_R3.digitalRead(P2) == HIGH;
+    if (pinIdx == 20) return _pcf8574_R3.digitalRead(P3) == HIGH;
+    if (pinIdx == 21) return _pcf8574_R3.digitalRead(P4) == HIGH;
+    if (pinIdx == 22) return _pcf8574_R3.digitalRead(P5) == HIGH;
+    if (pinIdx == 23) return _pcf8574_R3.digitalRead(P6) == HIGH;
+    if (pinIdx == 24) return _pcf8574_R3.digitalRead(P7) == HIGH;
+  }
+  if (pinIdx <= 32) {
+    if (pinIdx == 25) return _pcf8574_R4.digitalRead(P0) == HIGH;
+    if (pinIdx == 26) return _pcf8574_R4.digitalRead(P1) == HIGH;
+    if (pinIdx == 27) return _pcf8574_R4.digitalRead(P2) == HIGH;
+    if (pinIdx == 28) return _pcf8574_R4.digitalRead(P3) == HIGH;
+    if (pinIdx == 29) return _pcf8574_R4.digitalRead(P4) == HIGH;
+    if (pinIdx == 30) return _pcf8574_R4.digitalRead(P5) == HIGH;
+    if (pinIdx == 31) return _pcf8574_R4.digitalRead(P6) == HIGH;
+    if (pinIdx == 32) return _pcf8574_R4.digitalRead(P7) == HIGH;
+  }
+  return false;
+}
+void TA_Chip_KinCony_KC868_A32_R1_2::setDO_fr1_to32(int pinIdx, bool value) {
+  if (pinIdx <= 0) {
+    return;
+  }
+  if (pinIdx <= 8) {
+    if (pinIdx == 1) _pcf8574_R1.digitalWrite(P6, value ? HIGH : LOW);
+    if (pinIdx == 2) _pcf8574_R1.digitalWrite(P1, value ? HIGH : LOW);
+    if (pinIdx == 3) _pcf8574_R1.digitalWrite(P2, value ? HIGH : LOW);
+    if (pinIdx == 4) _pcf8574_R1.digitalWrite(P3, value ? HIGH : LOW);
+    if (pinIdx == 5) _pcf8574_R1.digitalWrite(P4, value ? HIGH : LOW);
+    if (pinIdx == 6) _pcf8574_R1.digitalWrite(P5, value ? HIGH : LOW);
+    if (pinIdx == 7) _pcf8574_R1.digitalWrite(P6, value ? HIGH : LOW);
+    if (pinIdx == 8) _pcf8574_R1.digitalWrite(P7, value ? HIGH : LOW);
+    return;
+  }
+  if (pinIdx <= 16) {
+    if (pinIdx == 9) _pcf8574_R2.digitalWrite(P0, value ? HIGH : LOW);
+    if (pinIdx == 10) _pcf8574_R2.digitalWrite(P1, value ? HIGH : LOW);
+    if (pinIdx == 11) _pcf8574_R2.digitalWrite(P2, value ? HIGH : LOW);
+    if (pinIdx == 12) _pcf8574_R2.digitalWrite(P3, value ? HIGH : LOW);
+    if (pinIdx == 13) _pcf8574_R2.digitalWrite(P4, value ? HIGH : LOW);
+    if (pinIdx == 14) _pcf8574_R2.digitalWrite(P5, value ? HIGH : LOW);
+    if (pinIdx == 15) _pcf8574_R2.digitalWrite(P6, value ? HIGH : LOW);
+    if (pinIdx == 16) _pcf8574_R2.digitalWrite(P7, value ? HIGH : LOW);
+    return;
+  }
+  if (pinIdx <= 24) {
+    if (pinIdx == 17) _pcf8574_R3.digitalWrite(P0, value ? HIGH : LOW);
+    if (pinIdx == 18) _pcf8574_R3.digitalWrite(P1, value ? HIGH : LOW);
+    if (pinIdx == 19) _pcf8574_R3.digitalWrite(P2, value ? HIGH : LOW);
+    if (pinIdx == 20) _pcf8574_R3.digitalWrite(P3, value ? HIGH : LOW);
+    if (pinIdx == 21) _pcf8574_R3.digitalWrite(P4, value ? HIGH : LOW);
+    if (pinIdx == 22) _pcf8574_R3.digitalWrite(P5, value ? HIGH : LOW);
+    if (pinIdx == 23) _pcf8574_R3.digitalWrite(P6, value ? HIGH : LOW);
+    if (pinIdx == 24) _pcf8574_R3.digitalWrite(P7, value ? HIGH : LOW);
+    return;
+  }
+  if (pinIdx <= 32) {
+    if (pinIdx == 25) _pcf8574_R4.digitalWrite(P0, value ? HIGH : LOW);
+    if (pinIdx == 26) _pcf8574_R4.digitalWrite(P1, value ? HIGH : LOW);
+    if (pinIdx == 27) _pcf8574_R4.digitalWrite(P2, value ? HIGH : LOW);
+    if (pinIdx == 28) _pcf8574_R4.digitalWrite(P3, value ? HIGH : LOW);
+    if (pinIdx == 29) _pcf8574_R4.digitalWrite(P4, value ? HIGH : LOW);
+    if (pinIdx == 30) _pcf8574_R4.digitalWrite(P5, value ? HIGH : LOW);
+    if (pinIdx == 31) _pcf8574_R4.digitalWrite(P6, value ? HIGH : LOW);
+    if (pinIdx == 32) _pcf8574_R4.digitalWrite(P7, value ? HIGH : LOW);
+    return;
+  }
+  return;
+}
+void TA_Chip_KinCony_KC868_A32_R1_2::setup() {
+  for (int i = 0; i <= 7; i++) {
+    _pcf8574_I1.pinMode(i, INPUT);
+    _pcf8574_I2.pinMode(i, INPUT);
+    _pcf8574_I3.pinMode(i, INPUT);
+    _pcf8574_I4.pinMode(i, INPUT);
+    _pcf8574_R1.pinMode(i, OUTPUT);
+    _pcf8574_R2.pinMode(i, OUTPUT);
+    _pcf8574_R3.pinMode(i, OUTPUT);
+    _pcf8574_R4.pinMode(i, OUTPUT);
+  }
+  String error = F("ERROR_pcf8574_");
+  if (_pcf8574_I1.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("I1"));
+  }
+  if (_pcf8574_I2.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("I2"));
+  }
+  if (_pcf8574_I3.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("I3"));
+  }
+  if (_pcf8574_I4.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("I4"));
+  }
+  if (_pcf8574_R1.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("R1"));
+  }
+  if (_pcf8574_R2.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("R2"));
+  }
+  if (_pcf8574_R3.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("R3"));
+  }
+  if (_pcf8574_R4.begin() == false) {
+    Serial.print(error);
+    Serial.println(F("R4"));
+  }
+  for (int i = 0; i <= 7; i++) {
+    _pcf8574_R1.digitalWrite(i, HIGH);
+    _pcf8574_R2.digitalWrite(i, HIGH);
+    _pcf8574_R3.digitalWrite(i, HIGH);
+    _pcf8574_R4.digitalWrite(i, HIGH);
+  }
+}
+String TA_Chip_KinCony_KC868_A32_R1_2::name() {
+  return String(ARDUINO_BOARD);
+}
+TA_Chip_KinCony_KC868_A32_R1_2 chip;
+
+//MAIN
+void setup() {
+  serial.setup();
+  chip.setup();
+
+  Serial.print(F("Chip.name: "));
+  Serial.print(chip.name());
+  Serial.print(F("\n"));
+
+  Serial.println(F("type hello"));
+}
+
+//THREAD
+void loop() {
+  //time.nextLoop(); NOT WORKING, NEED HELP
+  if (serial.hasNext()) forEach(serial.next());
+  delay(20);
+}
+
+void forEach(String command) {
+  //INIT
+  if (!command.startsWith(F("!"))) {
+    Serial.print(F("ERROR_CMD_TEMPLATE_UNKNOWN: "));
+    Serial.println(command);
+    return;
+  }
+
+  //COMMAND_NAME
+  TA_StringTokenizer tokens(command, F(" "));
+  if (!tokens.hasNext()) {
+    Serial.print(F("ERROR_CMD_UNCOMPLETE: "));
+    Serial.println(command);
+    return;
+  }
+  String cmdName = tokens.nextToken();
+
+  //IF COMMAND CHIP_NAME
+  if (cmdName.equals("!CHIP_NAME")) {
+    Serial.println(chip.name());
+    return;
+  }
+
+  //PIN_NUMBER_NAME
+  if (!tokens.hasNext()) {
+    Serial.print(F("ERROR_CMD_PIN_NUMBER_NAME_UNCOMPLETE: "));
+    Serial.println(command);
+    return;
+  }
+  String pinNumberName = tokens.nextToken();
+
+  //PIN_NUMBER
+  if (!stringUtils.isInt(pinNumberName)) {
+    Serial.print(F("ERROR_CMD_PIN_NAME_NOT_NUMBER: "));
+    Serial.println(command);
+    return;
+  }
+  int pinNumber = pinNumberName.toInt();
+
+  //IF COMMAND DO_GET
+  if (cmdName.equals("!DO_GET")) {
+    Serial.print(command);
+    Serial.print(F("->"));
+    Serial.println(chip.getDO_fr1_to32(pinNumber));
+    return;
+  }
+
+  //IF COMMAND DI_GET
+  if (cmdName.equals("!DI_GET")) {
+    Serial.print(command);
+    Serial.print(F("->"));
+    Serial.println(chip.getDI_fr1_to32(pinNumber));
+    return;
+  }
+
+  if (cmdName.equals("!DO_SET_TRUE")) {
+    chip.setDO_fr1_to32(pinNumber, true);
+    Serial.print(command);
+    Serial.println(F("-> DONE"));
+    return;
+  }
+
+  if (cmdName.equals("!DO_SET_FALSE")) {
+    chip.setDO_fr1_to32(pinNumber, false);
+    Serial.print(command);
+    Serial.println(F("-> DONE"));
+    return;
+  }
+
+  Serial.print(F("ERROR_CMD_NOT SUPPORTED: "));
+  Serial.println(command);
+  return;
 }
 
 
