48e310236c7919e02eefd5ab7ec9ad29a86055bb
tugalsan karabacak <tugalsan@gmail.com>
2023-04-18 12:08:04 +0300

    .

--- src/main/java/com/tugalsan/api/serialcom/server/test/chip/TS_SerialComChip_KinConyKC868_A32_R1_2_Timer.java
+++ src/main/java/com/tugalsan/api/serialcom/server/test/chip/TS_SerialComChip_KinConyKC868_A32_R1_2_Timer.java
@@ -1,6 +1,9 @@
 package com.tugalsan.api.serialcom.server.test.chip;
 
+import com.tugalsan.api.cast.client.TGS_CastUtils;
 import com.tugalsan.api.log.server.TS_Log;
+import com.tugalsan.api.string.server.TS_StringUtils;
+import java.util.stream.IntStream;
 
 public class TS_SerialComChip_KinConyKC868_A32_R1_2_Timer {
 
@@ -10,7 +13,7 @@ public class TS_SerialComChip_KinConyKC868_A32_R1_2_Timer {
         this.chip = chip;
     }
     final private TS_SerialComChip_KinConyKC868_A32_R1_2 chip;
-    final public int[] buffer = new int[32];
+    final private int[] buffer = new int[32];
 
     public static TS_SerialComChip_KinConyKC868_A32_R1_2_Timer of(TS_SerialComChip_KinConyKC868_A32_R1_2 chip) {
         return new TS_SerialComChip_KinConyKC868_A32_R1_2_Timer(chip);
@@ -22,18 +25,29 @@ public class TS_SerialComChip_KinConyKC868_A32_R1_2_Timer {
         if (reply.isEmpty()) {
             return false;
         }
-        var allValues = reply.get();
-        d.ce("refreshAll", "WHATTODO", allValues);
+        var resultString = reply.get();
+        var results = TS_StringUtils.toList_spc(resultString);
+        if (results.size() != 32) {
+            d.ce("refreshAll", "ERROR_SIZE_NOT_32", resultString, results);
+            return false;
+        }
+        for (var val : results) {
+            if (!TGS_CastUtils.isInteger(val)) {
+                d.ce("refreshAll", "ERROR_NOT_INT", val, resultString, results);
+                return false;
+            }
+        }
+        d.ci("refreshAll", results);
+        IntStream.range(0, 32).parallel().forEach(i -> {
+            buffer[i] = TGS_CastUtils.toInteger(results.get(i));
+        });
+        d.ci("refreshAll", results);
         return true;
-//        if (allValues.length() != 32) {
-//            d.ce("refreshAll", "ERROR_SIZE_NOT_32", reply, allValues);
-//            return false;
-//        }
-//        IntStream.rangeClosed(1, 32).parallel().forEach(pinNumber_fr1_to32 -> {
-//            timer(pinNumber_fr1_to32).setValueImitate(allValues.charAt(pinNumber_fr1_to32 - 1) == '1');
-//        });
-//        d.ci("refreshAll", "result", allValues);
-//        return true;
+    }
+
+    public int get(int pinNumber_fr1_to32) {
+        d.ci("get", "pinNumber_fr1_to32", pinNumber_fr1_to32);
+        return buffer[pinNumber_fr1_to32 - 1];
     }
 
     public boolean set(int pinNumber_fr1_to32, int secDuration) {
